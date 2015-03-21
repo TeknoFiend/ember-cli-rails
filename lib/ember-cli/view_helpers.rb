7b12b0b3aa6019db5d4b87042c4c282eb1f8b2eb
Weston Sewell <weston.sewell@gmail.com>
2015-03-20 21:47:05 -0500

    Fixed a couple syntax errors.

--- lib/ember-cli/view_helpers.rb
+++ lib/ember-cli/view_helpers.rb
@@ -10,11 +10,11 @@ module EmberCLI
       stylesheet_link_tag *app.exposed_css_assets
     end
 
-    def include_ember_alternative_stylesheet_tags(app_name, sources*)
+    def include_ember_alternative_stylesheet_tags(app_name, *sources)
       app = EmberCLI.configuration.apps.fetch(app_name)
 
       sources = sources.map do |source_or_option|
-        (source_or_option.is_a? Hash) ? source_or_option : "#{app.name}/source_or_option"
+        (source_or_option.is_a? Hash) ? source_or_option : "#{app.name}/#{source_or_option}"
       end
 
       stylesheet_link_tag *sources
